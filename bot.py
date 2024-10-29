ef9304536e0d7587c525692449c4ca2495867c6e
choqlito <161085180+juan4949@users.noreply.github.com>
2024-10-29 17:03:53 -0500

    commit

--- bot.py
+++ bot.py
@@ -18,27 +18,6 @@ def get_prefix(bot, message):
 
 bot = commands.Bot(command_prefix=get_prefix, intents=discord.Intents.all(), help_command=None)
 
-def load_translations():
-    """Carga las traducciones desde el archivo JSON."""
-    with open('translations.json', 'r', encoding='utf-8') as f:
-        return json.load(f)
-
-translations = load_translations()
-
-def get_translation(guild_id, key):
-    """Obtiene la traducción del mensaje en el idioma configurado."""
-    lang = bot.cogs['Language'].get_language(guild_id)
-    return translations[lang].get(key, key)
-
-@bot.event
-async def on_command_error(ctx, error):
-    """Maneja los errores de comandos y envía mensajes de error traducidos."""
-    error_message = str(error)
-    if isinstance(error, commands.CommandNotFound):
-        error_message = get_translation(ctx.guild.id, 'command_not_found')
-    elif isinstance(error, commands.BadArgument):
-        error_message = get_translation(ctx.guild.id, 'error_occurred')
-    await ctx.send(error_message)
 
 @bot.event
 async def on_ready():
@@ -59,11 +38,6 @@ async def on_message(message):
     """Procesa los comandos de los mensajes no bot."""
     if not message.author.bot:
         await bot.process_commands(message)
-
-@bot.command()
-async def test(ctx):
-    """Comando de prueba."""
-    await ctx.send(get_translation(ctx.guild.id, 'test_message'))
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 if TOKEN:
